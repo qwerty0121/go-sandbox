fec66bf26ee3142c02978a79eb1b1d58155c3297
qwerty0121 <sh1ng0718o@gmail.com>
2026-05-27 14:20:23 +0000

    課題3のソースレビューでの指摘対応

--- kadai/kadai03/main.go
+++ kadai/kadai03/main.go
@@ -10,7 +10,20 @@ type Account struct {
 	balance int
 }
 
+// NewAccount は、Accountのコンストラクタです。
+// 引数 initialBalance には口座の初期残高を指定します。
+func NewAccount(initialBalance int) *Account {
+	if initialBalance < 0 {
+		// 初期残高は常に0以上となるようにする
+		initialBalance = 0
+	}
+
+	return &Account{balance: initialBalance}
+}
+
 // Deposit は、指定された金額を口座に入金するメソッドです。
+// 引数 amount には入金する金額を指定します。
+// amount が正でない場合は何もしません。
 func (a *Account) Deposit(amount int) {
 	if amount <= 0 {
 		return
@@ -20,6 +33,9 @@ func (a *Account) Deposit(amount int) {
 }
 
 // Withdraw は、指定された金額を口座から出金するメソッドです。
+// 引数 amount には出金する金額を指定します。
+// 正常に出金できた場合は true を返します。
+// amount が正でない、または残高不足である場合は false を返します。
 func (a *Account) Withdraw(amount int) bool {
 	if amount <= 0 {
 		return false
@@ -34,21 +50,26 @@ func (a *Account) Withdraw(amount int) bool {
 	return true
 }
 
+// Balance は、口座の残高を返します。
+func (a *Account) Balance() int {
+	return a.balance
+}
+
 func main() {
 	// ケース1
-	account1 := Account{balance: 100}
+	account1 := NewAccount(100)
 	account1.Deposit(50)
-	fmt.Printf("account1's balance = %v\n", account1.balance)
+	fmt.Printf("account1's balance = %v\n", account1.Balance())
 
 	// ケース2
-	account2 := Account{balance: 150}
+	account2 := NewAccount(150)
 	result2 := account2.Withdraw(50)
 	fmt.Printf("account2 withdraw result = %v\n", result2)
-	fmt.Printf("account2's balance = %v\n", account2.balance)
+	fmt.Printf("account2's balance = %v\n", account2.Balance())
 
 	// ケース3
-	account3 := Account{balance: 100}
+	account3 := NewAccount(100)
 	result3 := account3.Withdraw(150)
 	fmt.Printf("account3 withdraw result = %v\n", result3)
-	fmt.Printf("account3's balance = %v\n", account3.balance)
+	fmt.Printf("account3's balance = %v\n", account3.Balance())
 }
